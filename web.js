0f8e25ddb9ffff74f1151b095435a09cb79be935
Daniel Naranjo <dnaranjo@gmail.com>
2013-07-07 17:52:00 +0000

    Redo all script, add buffer, add error message to debug

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
 
-response.send('Hello World 2!!');
+  var myfile = 'index.html';
+  var mybuffer = new buffer(256); 
+  var mydata;
 
-
+  fs.readFileSync(myfile, function(error, data) {
+    if(error) {
+	throw error;
+    }
+  mydata=data;
+  });
+  response.send(mydata);
+  
 });
 
 var port = process.env.PORT || 5000;
